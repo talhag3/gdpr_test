630c09490480c009370d5e1a023ba69cfcff0141
Talha Bilal <talha.bilal@sixlogics.com>
2020-10-01 14:50:12 +0500

    test

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 (function () {
     console.log("***************GDPR***************")
-    document.addEventListener("DOMContentLoaded", ()=>{
-        console.log("DOMContentLoaded")
-    });
     window.onload = function(){
         console.log("Window onload")
     }
+    
+    function checkConcent(){
+        window.__tcfapi('getTCData', 2, (tcData, success) => {
+            if(success) {
+              console.log(tcData)
+            } else {
+                console.log("cant get any tcData")
+            }
+        });
+    }
+
+
+
     console.log("***************GDPR***************")
 }());
