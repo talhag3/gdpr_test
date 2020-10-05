e1f797bee6eda6516c40e168ba9e4f587d033f66
Talha Bilal <talha.bilal@sixlogics.com>
2020-10-05 16:14:07 +0500

    test

--- index.php
+++ index.php
@@ -20,6 +20,32 @@
     </style>
 </head>
 <body>
+    <!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-fQr-P5f1jBwwe",
+uid:"__INSERT_EMAIL_HERE__"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-fQr-P5f1jBwwe.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
+
     <h1>Test Page</h1>
     <script src="./script.js"></script>
 </body>
