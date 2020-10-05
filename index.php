97de6200b0a12a90717ce8657e8413c2e6845720
Talha Bilal <talha.bilal@sixlogics.com>
2020-10-05 16:09:42 +0500

    test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page</title>
+    <style>
+        html, body
+        {
+            height: 100%;
+            background-color: black;
+        }
+        h1{
+            color: #00520e5e;
+            font-family: system-ui;
+            font-size: 72px;
+            text-align: center;
+            margin-top: 25%;
+        }
+    </style>
+</head>
+<body>
+    <h1>Test Page</h1>
+    <script src="./script.js"></script>
+</body>
+</html>
